itv3/homeproxy

commit 9b502cca2d199e34b2b5f1efc16ee3d611225ea6

refactor: remove *ray core support

root/etc/homeproxy/scripts/generate_sing-box.lua
@@ -143,12 +143,12 @@ local function generate_outbound(node)
 		-- Socks
 		version = node.socks_version,
 		-- VLESS / VMess
-		uuid = node.v2ray_uuid,
-		security = node.v2ray_vmess_encrypt,
+		uuid = node.uuid,
+		security = node.vmess_encrypt,
 		global_padding = node.vmess_global_padding and (node.vmess_global_padding == "1") or nil,
 		authenticated_length = node.vmess_authenticated_length and (node.vmess_authenticated_length == "1") or nil,
 		packet_addr = node.vmess_packet_addr and (node.vmess_packet_addr == "1") or nil,
-		packet_encoding = node.v2ray_packet_encoding,
+		packet_encoding = node.packet_encoding,
 		-- WireGuard
 		system_interface = (node.type == "wireguard") or nil,
 		interface_name = (node.type == "wireguard") and "emortal-wg-cfg-" .. node[".name"] .. "-out" or nil,
@@ -156,7 +156,7 @@ local function generate_outbound(node)
 		private_key = node.wireguard_private_key,
 		peer_public_key = node.wireguard_peer_public_key,
 		pre_shared_key = node.wireguard_pre_shared_key,
-		mtu = node.mkcp_mtu,
+		mtu = node.wireguard_mtu,
 
 		multiplex = (node.multiplex == "1") and {
 			enabled = true,
@@ -187,9 +187,9 @@ local function generate_outbound(node)
 		} or nil,
 		transport = notEmpty(node.transport) and {
 			type = node.transport,
-			host = node.h2_host or node.ws_host,
-			path = node.h2_path or node.ws_path,
-			method = node.h2_method,
+			host = node.http_host or node.ws_host,
+			path = node.http_path or node.ws_path,
+			method = node.http_method,
 			max_early_data = node.websocket_early_data,
 			early_data_header_name = node.websocket_early_data_header,
 			service_name = node.grpc_servicename
@@ -200,13 +200,6 @@ local function generate_outbound(node)
 		udp_fragment = (node.udp_fragment == "1") or nil
 	}
 	return outbound
-end
-
-local function generate_external_outbound(node)
-	if type(node) ~= "table" or isEmpty(node) then
-		return nil
-	end
-	-- todo
 end
 
 local function get_outbound(cfg)
@@ -513,20 +506,12 @@ config.outbounds = {
 -- Main outbounds
 if notEmpty(main_node) then
 	local main_node_cfg = uci:get_all(uciconfig, main_node) or {}
-	if main_node_cfg.type == "v2ray" then
-		config.outbounds[4] = generate_external_outbound(main_node_cfg)
-	else
-		config.outbounds[4] = generate_outbound(main_node_cfg)
-	end
+	config.outbounds[4] = generate_outbound(main_node_cfg)
 	config.outbounds[4].tag = "main-out"
 
 	if notEmpty(main_udp_node) and main_udp_node ~= "same" and main_udp_node ~= main_node then
 		local main_udp_node_cfg = uci:get_all(uciconfig, main_udp_node) or {}
-		if main_udp_node_cfg.type == "v2ray" then
-			config.outbounds[5] = generate_external_outbound(main_udp_node_cfg)
-		else
-			config.outbounds[5] = generate_outbound(main_udp_node_cfg)
-		end
+		config.outbounds[5] = generate_outbound(main_udp_node_cfg)
 		config.outbounds[5].tag = "main-udp-out"
 	end
 end
@@ -536,14 +521,10 @@ if notEmpty(default_outbound) then
 		if cfg.enabled == "1" then
 			local outbound = uci:get_all(uciconfig, cfg.node) or {}
 			local index = #config.outbounds + 1
-			if outbound.type == "v2ray" then
-				config.outbounds[index] = generate_external_outbound(outbound)
-			else
-				config.outbounds[index] = generate_outbound(outbound)
-				config.outbounds[index].domain_strategy = cfg.domain_strategy
-				config.outbounds[index].bind_interface = cfg.bind_interface
-				config.outbounds[index].detour = get_outbound(cfg.outbound)
-			end
+			config.outbounds[index] = generate_outbound(outbound)
+			config.outbounds[index].domain_strategy = cfg.domain_strategy
+			config.outbounds[index].bind_interface = cfg.bind_interface
+			config.outbounds[index].detour = get_outbound(cfg.outbound)
 			
 		end
 	end)

root/etc/homeproxy/scripts/update_subscribe.lua
@@ -187,7 +187,6 @@ local function parse_uri(uri)
 			config = {
 				label = uri.remarks,
 				type = "shadowsocks",
-				v2ray_protocol = notEmpty(uri.plugin) and "shadowsocks" or nil,
 				address = uri.server,
 				port = uri.server_port,
 				shadowsocks_encrypt_method = uri.method,
@@ -204,7 +203,7 @@ local function parse_uri(uri)
 			local params = url.query
 
 			if (not sing_features.with_quic) notEmpty(params.protocol) and params.protocol ~= "udp" then
-				log(translatef("Skipping unsupported %s node: %s.", "hysteria", urldecode(url.fragment, true) or url.host or "NULL"))
+				log(translatef("Skipping unsupported %s node: %s.", "hysteria", urldecode(url.fragment, true) or url.host))
 				return nil
 			end
 
@@ -250,7 +249,7 @@ local function parse_uri(uri)
 			end
 
 			if not table.contains(shadowsocks_encrypt_methods, userinfo[1]) then
-				log(translatef("Skipping unsupported %s node: %s.", "Shadowsocks", urldecode(url.fragment, true) or url.host or "NULL"))
+				log(translatef("Skipping unsupported %s node: %s.", "Shadowsocks", urldecode(url.fragment, true) or url.host))
 				return nil
 			end
 
@@ -284,7 +283,7 @@ local function parse_uri(uri)
 			local params = URL.parseQuery(uri[2]:gsub("^\?", ""))
 
 			if not sing_features.with_shadowsocksr then
-				log(translatef("Skipping unsupported %s node: %s.", "ShadowsocksR", b64decode(params.remarks) or userinfo[1] or "NULL"))
+				log(translatef("Skipping unsupported %s node: %s.", "ShadowsocksR", b64decode(params.remarks) or userinfo[1]))
 			end
 
 			config = {
@@ -317,50 +316,28 @@ local function parse_uri(uri)
 			local url = URL.parse("http://" .. uri[2])
 			local params = url.query
 
+			if (params.security == "xtls" or params.type == "kcp") then
+				log(translatef("Skipping unsupported %s node: %s.", "VLESS", urldecode(url.fragment, true) or url.host))
+			end
+
 			config = {
 				label = urldecode(url.fragment, true),
-				type = "v2ray",
-				v2ray_protocol = "vless",
+				type = "vless",
 				address = url.host,
 				port = url.port,
-				v2ray_uuid = url.user,
-				v2ray_vless_encrypt = params.encryption or "none",
-				v2ray_transport = params.type or "tcp",
+				uuid = url.user,
+				vless_encrypt = params.encryption or "none",
+				transport = (params.type ~= "tcp") and params.type or nil,
 				tls = (params.security == "tls") and "1" or "0",
 				tls_sni = params.sni,
-				tls_alpn = params.alpn and urldecode(params.alpn, true):split(",") or nil,
-				v2ray_xtls = (params.security == "xtls") and "1" or "0",
-				v2ray_xtls_flow = params.flow
+				tls_alpn = params.alpn and urldecode(params.alpn, true):split(",") or nil
 			}
-			if config.v2ray_transport == "grpc" then
+			if config.transport == "grpc" then
 				config.grpc_servicename = params.serviceName
-				config.grpc_mode = params.mode or "gun"
-			elseif config.v2ray_transport == "http" then
-				config.v2ray_transport = "h2"
-				config.h2_host = notEmpty(params.host) and urldecode(params.host, true):split(",")
-				config.h2_path = urldecode(params.path, true)
-			elseif config.v2ray_transport == "kcp" then
-				config.v2ray_transport = "mkcp"
-				config.mkcp_seed = params.seed
-				config.mkcp_header = params.headerType or "none"
-				-- Default settings from v2rayN
-				config.mkcp_downlink_capacity = "100"
-				config.mkcp_uplink_capacity = "12"
-				config.mkcp_read_buffer_size = "2"
-				config.mkcp_write_buffer_size = "2"
-				config.mkcp_mtu = "1350"
-				config.mkcp_tti = "50"
-			elseif config.v2ray_transport == "quic" then
-				config.quic_security = params.quicSecurity or "none"
-				config.quic_key = params.key
-				config.mkcp_header = params.headerType or "none"
-			elseif config.v2ray_transport == "tcp" then
-				config.tcp_header = notEmpty(params.headerType) or "none"
-				if config.tcp_header == "http" then
-					config.tcp_host = notEmpty(params.host) and urldecode(params.host, true):split(",") or nil
-					config.tcp_path = notEmpty(params.path) and urldecode(params.path, true):split(",") or nil
-				end
-			elseif config.v2ray_transport == "ws" then
+			elseif config.transport == "http" or params.headerType == "http" then
+				config.http_host = notEmpty(params.host) and urldecode(params.host, true):split(",") or nil
+				config.http_path = urldecode(params.path, true)
+			elseif config.transport == "ws" then
 				config.ws_host = (config.tls ~= "1") and urldecode(params.host, true) or nil
 				config.ws_path = urldecode(params.path, true)
 				if config.ws_path and config.ws_path:match("\?ed=") then
@@ -383,53 +360,30 @@ local function parse_uri(uri)
 				log(translatef("Skipping unsupported %s format.", "vmess"))
 				return nil
 			-- https://www.v2fly.org/config/protocols/vmess.html#vmess-md5-%E8%AE%A4%E8%AF%81%E4%BF%A1%E6%81%AF-%E6%B7%98%E6%B1%B0%E6%9C%BA%E5%88%B6
-			elseif notEmpty(uri.aid) and tonumber(uri.aid) ~= 0 then
-				log(translatef("Skipping legacy VMess node: %s.", notEmpty(uri.ps) or uri.add))
+			elseif (notEmpty(uri.aid) and tonumber(uri.aid) ~= 0) or params.type == "kcp" then
+				log(translatef("Skipping unsupported %s node: %s.", "VMess", notEmpty(uri.ps) or uri.add))
 				return nil
 			end
 
 			config = {
 				label = uri.ps,
-				type = "v2ray",
-				v2ray_protocol = "vmess",
+				type = "vmess",
 				address = uri.add,
 				port = uri.port,
-				v2ray_uuid = uri.id,
-				v2ray_vmess_encrypt = notEmpty(uri.scy) or "auto",
-				v2ray_transport = uri.net,
+				uuid = uri.id,
+				vmess_encrypt = notEmpty(uri.scy) or "auto",
+				transport = (uri.net ~= "tcp") and uri.net or nil,
 				tls = (uri.tls == "tls") and "1" or "0",
 				tls_sni = notEmpty(uri.sni) or uri.host,
 				tls_alpn = notEmpty(uri.alpn) and uri.alpn:split(",") or nil
 			}
-			if config.v2ray_transport == "grpc" then
+			if config.transport == "grpc" then
 				config.grpc_servicename = uri.path
-				config.grpc_mode = "gun"
-			elseif config.v2ray_transport == "h2" then
-				config.h2_host = notEmpty(uri.host) and uri.host:split(",") or nil
-				config.h2_path = uri.path
-			elseif config.v2ray_transport == "kcp" then
-				config.v2ray_transport = "mkcp"
-				config.mkcp_seed = uri.path
-				config.mkcp_header = notEmpty(uri.type) or "none"
-				-- Default settings from v2rayN
-				config.mkcp_downlink_capacity = "100"
-				config.mkcp_uplink_capacity = "12"
-				config.mkcp_read_buffer_size = "2"
-				config.mkcp_write_buffer_size = "2"
-				config.mkcp_mtu = "1350"
-				config.mkcp_tti = "50"
-			elseif config.v2ray_transport == "quic" then
-				config.quic_security = notEmpty(uri.host) or "none"
-				config.quic_key = uri.path
-				config.mkcp_header = notEmpty(uri.type) or "none"
-			elseif config.v2ray_transport == "tcp" then
-				config.tcp_header = (uri.type == "http") and "http" or "none"
-				if config.tcp_header == "http" then
-					config.tcp_header = uri.type
-					config.tcp_host = notEmpty(uri.host) and uri.host:split(",") or nil
-					config.tcp_path = notEmpty(uri.path) and uri.path:split(",") or nil
-				end
-			elseif config.v2ray_transport == "ws" then
+			elseif config.transport == "h2" or uri.type == "http" then
+				config.transport == "http"
+				config.http_host = notEmpty(uri.host) and uri.host:split(",") or nil
+				config.http_path = uri.path
+			elseif config.transport == "ws" then
 				config.ws_host = (config.tls ~= "1") and uri.host or nil
 				config.ws_path = uri.path
 				if notEmpty(config.ws_host) and config.ws_host:match("\?ed=") then
@@ -502,8 +456,8 @@ local function main()
 						if config.tls == "1" then
 							config.tls_insecure = allow_insecure
 						end
-						if config.type == "v2ray" and table.contains({"vless", "vmess"}, config.v2ray_protocol) then
-							config.v2ray_packet_encoding = packet_encoding
+						if table.contains({"vless", "vmess"}, config.type) then
+							config.packet_encoding = packet_encoding
 						end
 
 						config.grouphash = groupHash

Makefile
@@ -10,7 +10,6 @@ LUCI_TITLE:=The modern ImmortalWrt proxy platform for ARM64/AMD64
 LUCI_PKGARCH:=all
 LUCI_DEPENDS:= \
 	+sing-box \
-	+sagernet-core \
 	+curl \
 	+lua-neturl \
 	+kmod-inet-diag \

htdocs/luci-static/resources/view/homeproxy/client.js
@@ -92,8 +92,7 @@ return view.extend({
 		var proxy_nodes = {};
 		uci.sections(data[0], 'node', (res) => {
 			proxy_nodes[res['.name']] = 
-				String.format('[%s] %s', res.type === 'v2ray' ? res.type + '/' + res.v2ray_protocol : res.type,
-					res.label || res.server + ':' + res.server_port);
+				String.format('[%s] %s', res.type, res.label || res.server + ':' + res.server_port);
 		});
 
 		s = m.section(form.NamedSection, 'config', 'homeproxy');
@@ -244,15 +243,6 @@ return view.extend({
 			_('Outbound node'));
 		for (var i in proxy_nodes)
 			so.value(i, proxy_nodes[i]);
-		so.onchange = function(ev, section_id, value) {
-			var nodetype = uci.get(data[0], value, 'type');
-			var desc = this.map.findElement('id', 'cbid.homeproxy.%s.node'.format(section_id)).nextElementSibling;
-
-			if (!hp.native_protocols.includes(nodetype))
-				desc.innerHTML = _('<strong>The node you selected is not natively supported, the fields below are unavailable.</strong>');
-			else
-				desc.innerHTML = _('Outbound node');
-		}
 		so.validate = L.bind(hp.validateUniqueValue, this, data[0], 'routing_node', 'node');
 		so.editable = true;
 

htdocs/luci-static/resources/view/tools/homeproxy.js
@@ -31,15 +31,6 @@ return baseclass.extend({
 		'wireguard'
 	],
 
-	v2ray_native_protocols: [
-		'http',
-		'shadowsocks',
-		'socks',
-		'trojan',
-		'vless',
-		'vmess'
-	],
-
 	shadowsocks_encrypt_methods: [
 		/* Stream */
 		'none',

commit f1eef7ca92bf9718828f67c225595eec83f6d2cc

fix(node): add missing speed settings for hysteria
Fixes: 9b502cca2d199e ("refactor: remove *ray core support")
Fixes: #2

htdocs/luci-static/resources/view/homeproxy/node.js
@@ -41,8 +41,8 @@ function parseShareLink(uri, features) {
 				hysteria_auth_type: params.get('auth') ? 'string' : null,
 				hysteria_auth_payload: params.get('auth'),
 				hysteria_obfs_password: params.get('obfsParam'),
-				mkcp_downlink_capacity: params.get('downmbps'),
-				mkcp_uplink_capacity: params.get('upmbps'),
+				hysteria_down_mbps: params.get('downmbps'),
+				hysteria_up_mbps: params.get('upmbps'),
 				tls: '1',
 				tls_sni: params.get('peer'),
 				tls_alpn: params.get('alpn'),
@@ -529,6 +529,18 @@ return view.extend({
 		so.modalonly = true;
 
 		so = ss.option(form.Value, 'hysteria_obfs_password', _('Obfuscate password'));
+		so.depends('type', 'hysteria');
+		so.modalonly = true;
+
+		so = ss.option(form.Value, 'hysteria_down_mbps', _('Max download speed'),
+			_('Max download speed in Mbps.'));
+		so.datatype = 'uinteger';
+		so.depends('type', 'hysteria');
+		so.modalonly = true;
+
+		so = ss.option(form.Value, 'hysteria_up_mbps', _('Max upload speed'),
+			_('Max upload speed in Mbps.'));
+		so.datatype = 'uinteger';
 		so.depends('type', 'hysteria');
 		so.modalonly = true;
 

root/etc/homeproxy/scripts/generate_sing-box.lua
@@ -124,8 +124,8 @@ local function generate_outbound(node)
 		override_port = (node.type == "direct") and node.port or nil,
 		proxy_protocol = notEmpty(node.proxy_protocol) or nil,
 		-- Hysteria
-		up_mbps = node.mkcp_uplink_capacity,
-		down_mbps = node.mkcp_downlink_capacity,
+		up_mbps = node.hysteria_down_mbps,
+		down_mbps = node.hysteria_up_mbps,
 		obfs = node.hysteria_obfs_password,
 		auth = (node.hysteria_auth_type == "base64") and node.hysteria_auth_payload or nil,
 		auth_str = (node.hysteria_auth_type == "string") and node.hysteria_auth_payload or nil,

root/etc/homeproxy/scripts/update_subscribe.lua
@@ -199,8 +199,8 @@ local function parse_uri(uri)
 				hysteria_auth_type = params.auth and "string" or nil,
 				hysteria_auth_payload = params.auth,
 				hysteria_obfs_password = params.obfsParam,
-				mkcp_downlink_capacity = params.downmbps,
-				mkcp_uplink_capacity = params.upmbps,
+				hysteria_down_mbps = params.downmbps,
+				hysteria_up_mbps = params.upmbps,
 				tls = "1",
 				tls_insecure = table.contains({"true", "1"}, params.insecure) and "1" or "0",
 				tls_sni = params.peer,