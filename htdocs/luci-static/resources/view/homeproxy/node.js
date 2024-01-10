muink/luci-app-homeproxy

feat(node): Add new uTLS fingerprints

htdocs/luci-static/resources/view/homeproxy/node.js
@@ -1275,6 +1275,11 @@ return view.extend({
 			o.value('360', _('360'));
 			o.value('android', _('Android'));
 			o.value('chrome', _('Chrome'));
+			o.value('chrome_psk', _('Chrome PSK'));
+			o.value('chrome_psk_shuffle', _('Chrome PSK Shuffle'));
+			o.value('chrome_padding_psk_shuffle', _('Chrome Padding PSK Shuffle'));
+			o.value('chrome_pq', _('Chrome pq'));
+			o.value('chrome_pq_psk', _('Chrome pq PSK'));
 			o.value('edge', _('Edge'));
 			o.value('firefox', _('Firefox'));
 			o.value('ios', _('iOS'));

itv3/homeproxy

htdocs/luci-static/resources/view/homeproxy/node.js
@@ -1174,6 +1174,11 @@ return view.extend({
 			so.value('360', _('360'));
 			so.value('android', _('Android'));
 			so.value('chrome', _('Chrome'));
+			so.value('chrome_psk', _('Chrome PSK'));
+			so.value('chrome_psk_shuffle', _('Chrome PSK Shuffle'));
+			so.value('chrome_padding_psk_shuffle', _('Chrome Padding PSK Shuffle'));
+			so.value('chrome_pq', _('Chrome pq'));
+			so.value('chrome_pq_psk', _('Chrome pq PSK'));
 			so.value('edge', _('Edge'));
 			so.value('firefox', _('Firefox'));
 			so.value('ios', _('iOS'));