itv3/homeproxy

commit 50f905ef48e56748d895141708cd8ae3c707852c

feat(generator/client): add `proxy_protocol` option for `direct` outbound

root/etc/homeproxy/scripts/generate_client.uc
@@ -160,6 +160,10 @@ function generate_outbound(node) {
 		/* Direct */
 		override_address: node.override_address,
 		override_port: strToInt(node.override_port),
+		proxy_protocol: (node.proxy_protocol === '1') ? {
+			enabled: true,
+			version: strToInt(node.proxy_protocol_version)
+		} : null,
 		/* Hysteria (2) */
 		up_mbps: strToInt(node.hysteria_up_mbps),
 		down_mbps: strToInt(node.hysteria_down_mbps),

htdocs/luci-static/resources/view/homeproxy/node.js
@@ -468,6 +468,18 @@ function renderNodeSettings(section, data, features, main_node, routing_mode) {
 	o.datatype = 'port';
 	o.depends('type', 'direct');
 
+	o = s.option(form.Flag, 'proxy_protocol', _('Proxy protocol'),
+		_('Write proxy protocol in the connection header.'));
+	o.depends('type', 'direct');
+	o.modalonly = true;
+
+	o = s.option(form.ListValue, 'proxy_protocol_version', _('Proxy protocol version'));
+	o.value('1', _('v1'));
+	o.value('2', _('v2'));
+	o.default = '2';
+	o.depends('proxy_protocol', '1');
+	o.modalonly = true;
+
 	/* Hysteria (2) config start */
 	o = s.option(form.ListValue, 'hysteria_protocol', _('Protocol'));
 	o.value('udp');

commit 28f916696d629aca9e5208185b4a66616712d037

fix(generator): correct type for `proxy_protocol`
Fixes: 50f905ef48e5 feat(generator/client): add `proxy_protocol` option for `direct` outbound

root/etc/homeproxy/scripts/generate_client.uc
@@ -163,10 +163,7 @@ function generate_outbound(node) {
 		/* Direct */
 		override_address: node.override_address,
 		override_port: strToInt(node.override_port),
-		proxy_protocol: (node.proxy_protocol === '1') ? {
-			enabled: true,
-			version: strToInt(node.proxy_protocol_version)
-		} : null,
+		proxy_protocol: (node.proxy_protocol === '1') ? strToInt(node.proxy_protocol_version) : null,
 		/* Hysteria (2) */
 		up_mbps: strToInt(node.hysteria_up_mbps),
 		down_mbps: strToInt(node.hysteria_down_mbps),