itv3/homeproxy

commit 7f7b6ab96c466752df00192676e5250371e93cb3

chore(client): refine routing port settings

htdocs/luci-static/resources/view/homeproxy/client.js
@@ -234,14 +234,11 @@ return view.extend({
 
 		o = s.taboption('routing', form.Value, 'routing_port', _('Routing ports'),
 			_('Specify target ports to be proxied. Multiple ports must be separated by commas.'));
-		o.value('all', _('All ports'));
+		o.value('', _('All ports'));
 		o.value('common', _('Common ports only (bypass P2P traffic)'));
 		o.default = 'common';
-		o.rmempty = false;
 		o.validate = function(section_id, value) {
-			if (section_id && value !== 'all' && value !== 'common') {
-				if (!value)
-					return _('Expecting: %s').format(_('valid port value'));
+			if (section_id && value && value !== 'common') {
 
 				var ports = [];
 				for (var i of value.split(',')) {

root/etc/homeproxy/scripts/firewall_post.ut
@@ -50,7 +50,7 @@ if (routing_mode !== 'custom') {
 	bypass_cn_traffic = uci.get(cfgname, 'routing', 'bypass_cn_traffic') || '0';
 }
 
-let routing_port = uci.get(cfgname, 'config', 'routing_port') || 'common';
+let routing_port = uci.get(cfgname, 'config', 'routing_port');
 if (routing_port === 'common')
 	routing_port = uci.get(cfgname, 'infra', 'common_port') || '22,53,80,143,443,465,587,853,873,993,995,8080,8443,9418';
 
@@ -222,7 +222,7 @@ set homeproxy_wan_direct_addr_v6 {
 }
 {% endif /* ipv6_support */ %}
 
-{% if (routing_port !== 'all'): %}
+{% if (routing_port): %}
 set homeproxy_routing_port {
 	type inet_service
 	flags interval
@@ -248,7 +248,7 @@ chain homeproxy_redirect_proxy {
 }
 
 chain homeproxy_redirect_proxy_port {
-	{% if (routing_port !== 'all'): %}
+	{% if (routing_port): %}
 	tcp dport != @homeproxy_routing_port counter return
 	{% endif %}
 	goto homeproxy_redirect_proxy
@@ -363,14 +363,14 @@ chain homeproxy_mangle_tproxy {
 }
 
 chain homeproxy_mangle_tproxy_port {
-	{% if (routing_port !== 'all'): %}
+	{% if (routing_port): %}
 	udp dport != @homeproxy_routing_port counter return
 	{% endif %}
 	goto homeproxy_mangle_tproxy
 }
 
 chain homeproxy_mangle_mark {
-	{% if (routing_port !== 'all'): %}
+	{% if (routing_port): %}
 	udp dport != @homeproxy_routing_port counter return
 	{% endif %}
 	meta l4proto udp mark set {{ tproxy_mark }} counter accept
@@ -557,7 +557,7 @@ chain homeproxy_mangle_lanac {
 }
 
 chain homeproxy_mangle_tun_mark {
-	{% if (routing_port !== 'all'): %}
+	{% if (routing_port): %}
 	{% if (proxy_mode === 'tun'): %}
 	tcp dport != @homeproxy_routing_port counter return
 	{% endif /* proxy_mode */ %}

root/etc/uci-defaults/luci-homeproxy-migration
@@ -11,6 +11,11 @@ elif echo "$china_dns_server" | grep -q ","; then
 		uci -q add_list "homeproxy.config.china_dns_server"="$dns"
 	done
 fi
+
+if [ "$(uci -q get homeproxy.config.routing_port)" = "all" ]; then
+	uci -q delete "homeproxy.config.routing_port"
+fi
+
 [ -z "$(uci -q changes "homeproxy")" ] || uci -q commit "homeproxy"
 
 exit 0

commit 50d906cba10032ac14169664ed65e3af96f8a249

fix(client): dont set default value for routing port
Fixes: #186
Fixes: 7f7b6ab96c46 ("chore(client): refine routing port settings")
Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

htdocs/luci-static/resources/view/homeproxy/client.js
@@ -236,7 +236,6 @@ return view.extend({
 			_('Specify target ports to be proxied. Multiple ports must be separated by commas.'));
 		o.value('', _('All ports'));
 		o.value('common', _('Common ports only (bypass P2P traffic)'));
-		o.default = 'common';
 		o.validate = function(section_id, value) {
 			if (section_id && value && value !== 'common') {
 