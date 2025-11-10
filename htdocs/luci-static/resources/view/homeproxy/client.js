itv3/homeproxy

commit 18ebd868ffa9c350f8df33a2231c8b5175e2508e

fix(view/client): fix type of http/3 dns server
Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

htdocs/luci-static/resources/view/homeproxy/client.js
@@ -949,7 +949,7 @@ return view.extend({
 		so.value('tcp', _('TCP'));
 		so.value('tls', _('TLS'));
 		so.value('https', _('HTTPS'));
-		so.value('http3', _('HTTP3'));
+		so.value('h3', _('HTTP/3'));
 		so.value('quic', _('QUIC'));
 		so.default = 'udp';
 		so.rmempty = false;

commit 086f69e2bf18490170c22c499ab9d450f23561f3

fix(view/client): fix http/3 dns dependency
Fixes: 18ebd868ffa9 ("fix(view/client): fix type of http/3 dns server")
Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

htdocs/luci-static/resources/view/homeproxy/client.js
@@ -968,20 +968,20 @@ return view.extend({
 			_('The path of the DNS server.'));
 		so.placeholder = '/dns-query';
 		so.depends('type', 'https');
-		so.depends('type', 'http3');
+		so.depends('type', 'h3');
 		so.modalonly = true;
 
 		so = ss.option(form.DynamicList, 'headers', _('Headers'),
 			_('Additional headers to be sent to the DNS server.'));
 		so.depends('type', 'https');
-		so.depends('type', 'http3');
+		so.depends('type', 'h3');
 		so.modalonly = true;
 
 		so = ss.option(form.Value, 'tls_sni', _('TLS SNI'),
 			_('Used to verify the hostname on the returned certificates.'));
 		so.depends('type', 'tls');
 		so.depends('type', 'https');
-		so.depends('type', 'http3');
+		so.depends('type', 'h3');
 		so.depends('type', 'quic');
 		so.modalonly = true;
 