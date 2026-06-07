8f985e70999848c5f30b8fe5462eec88df550575
itv3 <itv3@users.noreply.github.com>
2026-06-07 16:28:38 +0800

    ci: version custom snapshot packages above feed builds

--- .github/build-ipk.sh
+++ .github/build-ipk.sh
@@ -23,7 +23,7 @@ PKG_NAME="$(get_mk_value "PKG_NAME")"
 if [ "$RELEASE_TYPE" == "release" ]; then
 	PKG_VERSION="$(get_mk_value "PKG_VERSION")"
 else
-	PKG_VERSION="$PKG_SOURCE_DATE_EPOCH~$(git rev-parse --short HEAD)"
+	PKG_VERSION="99.$PKG_SOURCE_DATE_EPOCH~$(git rev-parse --short HEAD)"
 fi
 
 TEMP_DIR="$(mktemp -d -p $BASE_DIR)"
